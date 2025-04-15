f72305c1cfa6f1b5cb280cc65f4cf32dfb47e616
binhantt <doan44503@gmail.com>
2025-04-15 20:00:17 +0700

    ds

---
+++ src/config/data/skills.js
@@ -0,0 +1,18 @@
+export const skillsData = {
+    frontendSkills: [
+        { name: 'Vue.js', level: 90, icon: 'fab fa-vuejs' },
+        { name: 'React', level: 85, icon: 'fab fa-react' },
+        { name: 'JavaScript', level: 90, icon: 'fab fa-js' },
+        { name: 'HTML5', level: 95, icon: 'fab fa-html5' },
+        { name: 'CSS3', level: 90, icon: 'fab fa-css3-alt' },
+        { name: 'Bootstrap', level: 85, icon: 'fab fa-bootstrap' }
+    ],
+    backendSkills: [
+        { name: 'Node.js', level: 80, icon: 'fab fa-node-js' },
+        // { name: 'Python', level: 75, icon: 'fab fa-python' },
+        { name: 'PHP', level: 70, icon: 'fab fa-php' },
+        { name: 'MySQL', level: 75, icon: 'fas fa-database' },
+        { name: 'MongoDB', level: 70, icon: 'fas fa-leaf' },
+        // { name: 'Docker', level: 65, icon: 'fab fa-docker' }
+    ]
+}
--- src/views/skill_view.vue
+++ src/views/skill_view.vue
@@ -23,27 +23,13 @@
 import { defineOptions } from 'vue'
 import Skills from '@/components/base/Skills.vue'
 import MainLayout from '@/components/Layout/main_layout.vue'
+import { skillsData } from '@/config/data/skills'
 import { ref } from 'vue'
 
 defineOptions({
     name: 'SkillView',
 })
 
-const frontendSkills = ref([
-    { name: 'Vue.js', level: 90, icon: 'fab fa-vuejs' },
-    { name: 'React', level: 85, icon: 'fab fa-react' },
-    { name: 'JavaScript', level: 90, icon: 'fab fa-js' },
-    { name: 'HTML5', level: 95, icon: 'fab fa-html5' },
-    { name: 'CSS3', level: 90, icon: 'fab fa-css3-alt' },
-    { name: 'Bootstrap', level: 85, icon: 'fab fa-bootstrap' }
-])
-
-const backendSkills = ref([
-    { name: 'Node.js', level: 80, icon: 'fab fa-node-js' },
-    { name: 'Python', level: 75, icon: 'fab fa-python' },
-    { name: 'PHP', level: 70, icon: 'fab fa-php' },
-    { name: 'MySQL', level: 75, icon: 'fas fa-database' },
-    { name: 'MongoDB', level: 70, icon: 'fas fa-leaf' },
-    { name: 'Docker', level: 65, icon: 'fab fa-docker' }
-])
+const frontendSkills = ref(skillsData.frontendSkills)
+const backendSkills = ref(skillsData.backendSkills)
 </script>
